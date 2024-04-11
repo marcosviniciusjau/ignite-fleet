8550d620e1c5f3c1c68148a9f1de94f0b40ad5a4
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-04-11 15:43:34 -0300

    creating the signIn page

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,10 +1,23 @@
-import { StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
 
-export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+export const Container = styled.ImageBackground`
+  flex: 1;
+  align-items: center;
+  padding:52px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_800};
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.COLORS.BRAND_LIGHT};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XXXL}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  margin-top: 40px;
+`;
+
+export const Slogan = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_100};
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  margin-bottom:32px;
+`;
